8bf178115a0bf9d779a56171555dfa3a31ae30a0
HikarusV <nazwapradana.pc@gmail.com>
2022-06-21 00:27:03 +0700

    Add System Notification Hanlder and Background Service Handler.

---
+++ lib/common/utils/system_notification.dart
@@ -0,0 +1,97 @@
+import 'dart:ffi';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rxdart/subjects.dart';
+
+class NotificationSystem {
+  static final selectNotificationSubject = BehaviorSubject<String>();
+
+  static late FlutterLocalNotificationsPlugin _notifications;
+
+  static late AndroidNotificationChannel channel;
+
+  static const initAndroidSetting =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  static const initSetting = InitializationSettings(
+    android: initAndroidSetting,
+  );
+
+  static Future init({
+    required FlutterLocalNotificationsPlugin initNotifPlugin,
+    required AndroidNotificationChannel androidChannel,
+  }) async {
+    _notifications = initNotifPlugin;
+    channel = androidChannel;
+
+    await _notifications.initialize(
+      initSetting,
+      onSelectNotification: (payload) async {
+        selectNotificationSubject.add(payload!);
+        print(payload);
+      },
+    );
+
+    FirebaseMessaging.onMessage.listen(
+      (RemoteMessage message) {
+        RemoteNotification? remoteNotification = message.notification;
+        AndroidNotification? androidNotification =
+            message.notification?.android;
+
+        if (remoteNotification != null && androidNotification != null) {
+          final data = remoteNotification;
+          showNotif(
+            data.title!,
+            data.body!,
+            'payload firebase',
+            id: data.hashCode,
+          );
+        }
+      },
+    );
+
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {
+      print('ada pesan dari firebase : ${event.notification?.title ?? 'none'}');
+    });
+  }
+
+  static void showNotif(
+    String title,
+    String body,
+    String payload, {
+    int id = 0,
+  }) async {
+    await _notifications.show(
+      id,
+      title,
+      body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          priority: Priority.max,
+          ticker: 'ticker',
+          color: Colors.blue,
+        ),
+      ),
+      payload: payload,
+    );
+  }
+
+  // static Future<bool> getAppLaunchFromNotifDetails() async {
+  //   final details = await _notifications.getNotificationAppLaunchDetails();
+  //   if (details != null && details.didNotificationLaunchApp) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+  //
+  // static Future<String> getNotificationsPayload() async {
+  //   final details = await _notifications.getNotificationAppLaunchDetails();
+  //   return (details?.payload) ?? 'none';
+  // }
+}
